9125535c36eef0202fdd94e7dd0315b707a75b4d
Eric Stern <eric@ericstern.com>
2018-01-01 12:39:49 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,105 @@
+/*
+ * Expected config:
+ *
+ * {
+ *   "platform": "YamahaAVR",
+ *   "host": "192.168.1.45",
+ * }
+ */
+var Accessory, Characteristic, Service, UUIDGen;
+
+const platformName = 'homebridge-plugin-yamaha';
+const platformPrettyName = 'YamahaAVR';
+// const fetch = require('node-fetch');
+const Yamaha = require('yamaha-nodejs');
+
+module.exports = (homebridge) => {
+  Accessory = homebridge.platformAccessory;
+  Characteristic = homebridge.hap.Characteristic;
+  Service = homebridge.hap.Service;
+  UUIDGen = homebridge.hap.uuid;
+
+  homebridge.registerAccessory(platformName, platformPrettyName, YamahaAVR, true);
+};
+
+class YamahaAVR {
+  // These values are provided via Homebridge
+  constructor(log, config, api) {
+    if (!config) {
+      log('Ignoring HDMI switch - no config');
+      return;
+    }
+    this.log = log;
+    this.api = api;
+    const { host } = config;
+
+    this.yamaha = new Yamaha(host);
+
+    [this.infoService, this.speakerService, this.switchService] = this.createServices();
+  }
+ 
+  createServices = () => {
+    const infoService = new Service.AccessoryInformation();
+    infoService
+      .setCharacteristic(Characteristic.Manufacturer, 'Yamaha')
+      .setCharacteristic(Characteristic.Model, 'TODO Model')
+      .setCharacteristic(Characteristic.SerialNumber, 'TODO SN');
+
+    const speakerService = new Service.Speaker();
+    speakerService
+      .getCharacteristic(Characteristic.Mute)
+      .on('get', this.getMute)
+      .on('set', this.setMute);
+    speakerService
+      .getCharacteristic(Characteristic.Volume)
+      .on('get', this.getVolume)
+      .on('set', this.setVolume);
+    speakerService
+      .setCharacteristic(Characteristic.Name, 'Yamaha Receiver');
+
+    const switchService = new Service.Switch(this.name);
+    switchService
+      .getCharacteristic(Characteristic.On)
+      .on('get', this.getPower)
+      .on('set', this.setPower);
+
+    return [infoService, speakerService, switchService];
+  }
+
+  getServices = () => {
+    return [
+      this.infoService,
+      this.speakerService,
+      this.switchService,
+    ]
+  }
+
+  getMute = (cb) => this.yamaha
+    .getBasicInfo()
+    .done(info => info.isMuted().then(muted => cb(null, muted)));
+
+  setMute = (muted, cb) => {
+    this.log('setmute ' + muted);
+    cb();
+  }
+
+  getVolume = (cb) => {
+    this.log('getvolume');
+    cb(40);
+  }
+
+  setVolume = (vol, cb) => {
+    this.log('setVolume ' + vol);
+    cb();
+  }
+
+  getPower = (cb) => {
+    cb(null, true);
+  }
+
+  setPower = (on, cb) => {
+    this.log('setpower ' + on);
+    cb();
+  }
+
+}
